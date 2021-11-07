9822b46b8beef24c0dac732bee23ef023fadb9b3
danvasem <davaca@maint.com.ec>
2021-11-06 23:09:07 -0500

    Initial version

---
+++ contracts/HelloBlockchain.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloBlockchain{
+
+    function sayHi() public pure returns (string memory){
+        return "Hola Blockchain!!";
+    }
+    
+}
